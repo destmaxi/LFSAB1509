2fed4a44e22edd8e8a824d3ff23b21c071522968
destmaxi <maxime.destreel@student.uclouvain.be>
2018-03-02 13:42:06 +0100

    add KEEP_SCREEN_ON

--- android/src/be/ucl/lfsab1509/gravityrun/AndroidLauncher.java
+++ android/src/be/ucl/lfsab1509/gravityrun/AndroidLauncher.java
@@ -1,21 +1,22 @@
 package be.ucl.lfsab1509.gravityrun;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import be.ucl.lfsab1509.gravityrun.GravityRun;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		AndroidApplicationConfiguration config;
 		config = new AndroidApplicationConfiguration();
 		config.useGyroscope = true;  //default is false
 
 		config.useAccelerometer = false;
 		config.useCompass = false;
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		initialize(new GravityRun(), config);
 	}
 }

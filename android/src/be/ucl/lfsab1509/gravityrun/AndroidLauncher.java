a26b23c6abdc805cbea8bb64d3ff3ac237b2a405
destmaxi <maxime.destreel@student.uclouvain.be>
2018-03-09 13:49:38 +0100

    add crashlytics to app

--- android/src/be/ucl/lfsab1509/gravityrun/AndroidLauncher.java
+++ android/src/be/ucl/lfsab1509/gravityrun/AndroidLauncher.java
@@ -5,14 +5,18 @@ import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Fabric.with(this, new Crashlytics());
 		AndroidApplicationConfiguration config;
 		config = new AndroidApplicationConfiguration();
 		config.useGyroscope = true;  //default is false
+
 
 		config.useAccelerometer = true;
 		config.useCompass = false;

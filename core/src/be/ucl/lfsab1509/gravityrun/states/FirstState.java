62e572e17ea326105da9eecfed1833d81792cfc0
Cédric De Bontridder <cedric.debontridder@student.uclouvain.be>
2018-03-25 17:12:32 +0200

    Hide keyboard after FirstState

--- core/src/be/ucl/lfsab1509/gravityrun/states/FirstState.java
+++ core/src/be/ucl/lfsab1509/gravityrun/states/FirstState.java
@@ -2,7 +2,9 @@ package be.ucl.lfsab1509.gravityrun.states;
 
 import be.ucl.lfsab1509.gravityrun.GravityRun;
 import be.ucl.lfsab1509.gravityrun.tools.Skin;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -16,6 +18,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 import java.util.ArrayList;
 
 public class FirstState extends State {
@@ -33,7 +36,7 @@ public class FirstState extends State {
         float ch = h * 0.9f;
 
         errorSkin = new Skin();
-        errorSkin.createSkin(28);
+        errorSkin.createSkin((int) (0.75f * w / d / 10));
         errorLabel = new Label(string.format("error"), errorSkin, "error");
         errorLabel.setWrap(true);
         errorLabel.setWidth(cw);
@@ -41,11 +44,11 @@ public class FirstState extends State {
         errorLabel.setVisible(false);
 
         menuSkin = new Skin();
-        menuSkin.createSkin(62);
+        menuSkin.createSkin((int) (1.5f * w / d / 10));
         Label title = new Label(string.format("welcome"), menuSkin, "title");
 
         tableSkin = new Skin();
-        tableSkin.createSkin(42);
+        tableSkin.createSkin((int) (w / d / 10));
         TextButton startButton = new TextButton(string.format("start"), tableSkin, "round");
         startButton.addListener(new ClickListener() {
             @Override
@@ -62,7 +65,6 @@ public class FirstState extends State {
             }
         });
         usernameField.addListener(new ChangeListener() {
-
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 username = usernameField.getText();
@@ -73,7 +75,7 @@ public class FirstState extends State {
         Table table = new Table();
 
         tableContainer.setSize(cw, ch);
-        tableContainer.setPosition((w - cw) / 2,(h - ch) / 2);
+        tableContainer.setPosition((w - cw) / 2, (h - ch) / 2);
         tableContainer.top().fillX();
         tableContainer.setActor(table);
 
@@ -95,30 +97,23 @@ public class FirstState extends State {
     @Override
     protected void handleInput() {
         if (isClickedStartButton) {
-            GravityRun.user.setUsername(username);
-            GravityRun.user.setFirstTimeTrue();
-            GravityRun.user.setBeginner(new ArrayList<Integer>());
-            GravityRun.user.setInter(new ArrayList<Integer>());
-            GravityRun.user.setExpert(new ArrayList<Integer>());
-            GravityRun.user.setIndexSelected(0);
-            ArrayList<Integer> arrayList = new ArrayList<Integer>();
-
-            for(int i = 0; i < 3; i++)
-                arrayList.add(0);
-
-            GravityRun.user.setHighScore(arrayList);
-
-            GravityRun.pref.put(GravityRun.user.toMap());
-            GravityRun.pref.flush();
-
-            gsm.set(new MenuState(gsm));
+            Gdx.input.setOnscreenKeyboardVisible(false);
+            if (username.equals(string.format("username"))) {
+                isClickedStartButton = false;
+                errorLabel.setVisible(true);
+            } else {
+                initUser();
+                gsm.set(new MenuState(gsm));
+            }
         }
+        
+        if (Gdx.input.isKeyJustPressed(Input.Keys.BACK) || Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE))
+            Gdx.app.exit();
     }
 
     @Override
     public void update(float dt) {
-        if (isClickedStartButton && !username.equals(string.format("username")))
-            handleInput();
+        handleInput();
     }
 
     @Override
@@ -126,20 +121,31 @@ public class FirstState extends State {
         sb.setProjectionMatrix(cam.combined);
         stage.act();
         stage.draw();
-        if (isClickedStartButton && !username.equals(string.format("username"))) {
-            gsm.update(Gdx.graphics.getDeltaTime());
-        }
-        else if (isClickedStartButton) {
-            isClickedStartButton = false;
-            errorLabel.setVisible(true);
-        }
     }
 
     @Override
     public void dispose() {
-        tableSkin.dispose();
         errorSkin.dispose();
         menuSkin.dispose();
         stage.dispose();
+        tableSkin.dispose();
     }
+
+    private void initUser() {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (int i = 0; i < 3; i++)
+            arrayList.add(0);
+
+        GravityRun.user.setUsername(username);
+        GravityRun.user.setFirstTimeTrue();
+        GravityRun.user.setBeginner(new ArrayList<Integer>());
+        GravityRun.user.setInter(new ArrayList<Integer>());
+        GravityRun.user.setExpert(new ArrayList<Integer>());
+        GravityRun.user.setIndexSelected(0);
+        GravityRun.user.setHighScore(arrayList);
+
+        GravityRun.pref.put(GravityRun.user.toMap());
+        GravityRun.pref.flush();
+    }
+
 }

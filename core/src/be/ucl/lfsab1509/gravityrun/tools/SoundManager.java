93bfe6b5fba24bacc922e2db70ff5422f810b792
VanStratum <arthur.van.stratum@gmail.com>
2018-03-25 17:40:40 +0200

    Add a sound manager

---
+++ core/src/be/ucl/lfsab1509/gravityrun/tools/SoundManager.java
@@ -0,0 +1,47 @@
+package be.ucl.lfsab1509.gravityrun.tools;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
+/**
+ * Manage the creation, deletion and playing of sounds and music.
+ */
+
+public class SoundManager {
+    private Music menuMusic;
+    private Music gameMusic;
+    private Sound marbleBreak;
+
+    public SoundManager(){
+        this.gameMusic = Gdx.audio.newMusic(Gdx.files.internal("sound/6_symphony.mp3"));
+        this.menuMusic = Gdx.audio.newMusic(Gdx.files.internal("sound/2_suite_holst.mp3"));
+        gameMusic.setLooping(true);
+        menuMusic.setLooping(true);
+        this.marbleBreak = Gdx.audio.newSound(Gdx.files.internal("sound/break.wav"));
+    }
+
+    public void playMenu(){
+        if (gameMusic.isPlaying())
+            gameMusic.pause();
+        if (!menuMusic.isPlaying())
+            menuMusic.play();
+    }
+
+    public void playGame(){
+        if (menuMusic.isPlaying())
+            menuMusic.pause();
+        if (!gameMusic.isPlaying())
+            gameMusic.play();
+    }
+
+    public void marbleBreak(){
+        marbleBreak.play();
+    }
+
+    public void dispose(){
+        menuMusic.dispose();
+        gameMusic.dispose();
+        marbleBreak.dispose();
+    }
+}

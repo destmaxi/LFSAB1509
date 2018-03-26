0530db72dda20829592ea75072820ab0c02898a7
VanStratum <arthur.van.stratum@gmail.com>
2018-03-26 20:51:11 +0200

    Add a way to change the sound level in Soundmanager
    Correct the accidental deletion of the beginning of the music in
    GravityRun.java

--- core/src/be/ucl/lfsab1509/gravityrun/tools/SoundManager.java
+++ core/src/be/ucl/lfsab1509/gravityrun/tools/SoundManager.java
@@ -6,9 +6,10 @@ import com.badlogic.gdx.audio.Sound;
 
 public class SoundManager {
 
-    private final float soundLevel = 0.5f;
     private Music gameMusic, menuMusic;
     private Sound bonus, marbleBreak;
+    private float soundLevel;
+    private float musicLevel;
 
     public SoundManager() {
         this.bonus = Gdx.audio.newSound(Gdx.files.internal("sounds/bonus.wav"));
@@ -17,7 +18,9 @@ public class SoundManager {
         this.menuMusic = Gdx.audio.newMusic(Gdx.files.internal("sounds/Menu.mp3"));
         gameMusic.setLooping(true);
         menuMusic.setLooping(true);
-        gameMusic.setVolume(0.8f);
+        musicLevel = 0.8f;
+        soundLevel = 0.5f;
+	setMusicLevel(musicLevel);
     }
 
     // called when simply exiting the app with back button
@@ -38,6 +41,30 @@ public class SoundManager {
             marbleBreak.play(soundLevel);
     }
 
+    public void setMusicLevel(float musicLevel){
+        this.musicLevel = musicLevel;
+        gameMusic.setVolume(musicLevel);
+        menuMusic.setVolume(musicLevel);
+    }
+
+    public void setSoundLevel(float soundLevel){
+        this.soundLevel = soundLevel;
+    }
+
+    public void playGame() {
+        if (menuMusic.isPlaying())
+            menuMusic.pause();
+        if (!gameMusic.isPlaying())
+            gameMusic.play();
+    }
+
+    public void playMenu() {
+        if (gameMusic.isPlaying())
+            gameMusic.pause();
+        if (!menuMusic.isPlaying())
+            menuMusic.play();
+    }
+
     public void replayGame() {
         if (menuMusic.isPlaying())
             menuMusic.pause();
@@ -55,5 +82,4 @@ public class SoundManager {
             menuMusic.play();
         }
     }
-
 }

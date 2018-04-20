b77bc5aca60e2d32c71df0590661f42e953ac065
Cédric De Bontridder <cedric.debontridder@student.uclouvain.be>
2018-04-20 23:54:41 +0200

    Fix GpgsMappers not handling null Strings

--- core/src/be/ucl/lfsab1509/gravityrun/tools/GpgsMappers.java
+++ core/src/be/ucl/lfsab1509/gravityrun/tools/GpgsMappers.java
@@ -3,6 +3,9 @@ package be.ucl.lfsab1509.gravityrun.tools;
 public class GpgsMappers {
 
     public static String mapToGpgsEvent(String eventId) {
+        if (eventId == null)
+            return null;
+
         String gpgsId;
 
         // TODO
@@ -18,6 +21,9 @@ public class GpgsMappers {
     }
 
     public static String mapToGpgsAchievement(String independantId) {
+        if (independantId == null)
+            return null;
+
         String gpgsId;
 
         // TODO
@@ -33,6 +39,9 @@ public class GpgsMappers {
     }
 
     public static String mapToGpgsLeaderBoard(String independantId) {
+        if (independantId == null)
+            return null;
+
         String gpgsId;
 
         // TODO
